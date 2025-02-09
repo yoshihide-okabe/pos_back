5e53c1f11d551132af3532865133b9f3e54122d5
yoshihide-okabe <hidechan.okabe@gmail.com>
2025-02-09 14:52:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def read_root():
 # MySQL接続設定
 db_config = {
     "host": "tech0-gen-8-step4-db-5.mysql.database.azure.com",
-    "user": "Tech0Gen8TA5@tech0-gen-8-step4-db-5",
+    "user": "Tech0Gen8TA5",
     "password": "gen8-1-ta@5",
     "database": "class5_db",
     "ssl_ca": "C:\\MySQL\\MySQL Server 8.4\\certs\\DigiCertGlobalRootCA.crt.pem"  # ✅ 余計なカンマを削除
