80ac17ebf0e8d95c0c38148904fd976c1eb40dd5
yoshihide-okabe <hidechan.okabe@gmail.com>
2025-02-09 18:39:23 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --capture-output --log-level=info
+
